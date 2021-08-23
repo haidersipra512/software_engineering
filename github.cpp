56dceebd4127f6a0f47da3bf731fcf8abce48643
haidersipra512 <89371359+haidersipra512@users.noreply.github.com>
2021-08-23 09:27:48 +0500

    Add files via upload

---
+++ github.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout << "hello, whats your name?" << endl;
+	cin >> name;
+	cout << "it was nice to know your name"<< ":" << name << endl;
+	return 0;
+}
+
